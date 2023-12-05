b50e7523779d9b64fed4b9c2835d7d6d2700b0b3
George Williams <george.williams@gmail.com>
2023-12-05 08:20:39 -0800

    dedicated script for 1B

---
+++ gsi/run_gxl_1B.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+set -x
+
+#
+# setup and config
+#
+
+# create timestamped output dir
+TIMESTAMP=$(date +%s)
+OUTPUT="/tmp/gxl_$TIMESTAMP"
+mkdir -p $OUTPUT
+
+#
+# Run GXL commands
+#
+
+# run GXL on 1B
+DATASET="deep-1000M"
+echo "Running GXL on $DATASET"
+echo "Writing output to $OUTPUT"
+python gxl_bench.py --dataset $DATASET --output $OUTPUT | tee "$OUTPUT/$DATASET.log"
