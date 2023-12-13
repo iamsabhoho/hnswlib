45662eaec52ab7e0ccf12bb4575d16355eb6fbfb
George Williams <george.williams@gmail.com>
2023-12-13 18:55:47 +0000

    sync before running on apu3

--- gsi/run_gxl_apu3.sh
+++ gsi/run_gxl_apu3.sh
@@ -22,40 +22,40 @@ echo "Running GXL on $DATASET"
 echo "Writing output to $OUTPUT"
 python -u gxl_bench.py --dataset $DATASET --output "$OUTPUT/$DATASET" | tee "$OUTPUT/$DATASET.log" 
 
-exit 1
-
 # run GXL on 10M
 DATASET="deep-10M"
 echo "Running GXL on $DATASET"
 echo "Writing output to $OUTPUT"
-python gxl_bench.py --dataset $DATASET --output $OUTPUT  | tee "$OUTPUT/$DATASET.log"
+python gxl_bench.py --dataset $DATASET --output "$OUTPUT/$DATASET"  | tee "$OUTPUT/$DATASET.log"
 
 # run GXL on 20M
 DATASET="deep-20M"
 echo "Running GXL on $DATASET"
 echo "Writing output to $OUTPUT"
-python gxl_bench.py --dataset $DATASET --output $OUTPUT  | tee "$OUTPUT/$DATASET.log"
+python gxl_bench.py --dataset $DATASET --output "$OUTPUT/$DATASET"  | tee "$OUTPUT/$DATASET.log"
 
 # run GXL on 50M
 DATASET="deep-50M"
 echo "Running GXL on $DATASET"
 echo "Writing output to $OUTPUT"
-python gxl_bench.py --dataset $DATASET --output $OUTPUT | tee "$OUTPUT/$DATASET.log"
+python gxl_bench.py --dataset $DATASET --output "$OUTPUT/$DATASET" | tee "$OUTPUT/$DATASET.log"
 
 # run GXL on 100M
 DATASET="deep-50M"
 echo "Running GXL on $DATASET"
 echo "Writing output to $OUTPUT"
-python gxl_bench.py --dataset $DATASET --output $OUTPUT | tee "$OUTPUT/$DATASET.log"
+python gxl_bench.py --dataset $DATASET --output "$OUTPUT/$DATASET" | tee "$OUTPUT/$DATASET.log"
+
+exit 1
 
 # run GXL on 250M
 DATASET="deep-250M" 
 echo "Running GXL on $DATASET"
 echo "Writing output to $OUTPUT"
-python gxl_bench.py --dataset $DATASET --output $OUTPUT  | tee "$OUTPUT/$DATASET.log"
+python gxl_bench.py --dataset $DATASET --output "$OUTPUT/$DATASET"  | tee "$OUTPUT/$DATASET.log"
 
 # run GXL on 500M
 DATASET="deep-500M"
 echo "Running GXL on $DATASET"
 echo "Writing output to $OUTPUT"
-python gxl_bench.py --dataset $DATASET --output $OUTPUT | tee "$OUTPUT/$DATASET.log"
+python gxl_bench.py --dataset $DATASET --output "$OUTPUT/$DATASET" | tee "$OUTPUT/$DATASET.log"
